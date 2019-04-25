986fd78a08115ac34ad1fcc15d9d5c3dea11ca94
Vadzim <vadzimk@hotmail.com>
2019-04-24 20:11:23 -0700

    added a slider to Home

---
+++ client/src/components/Home/HomeSlider.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import Slider from 'react-slick';
+import MyButton from '../utils/MyButton';
+
+const HomeSlider = (props) => {
+    //gives an array of slides (later will be fetched from the server database.
+    const slides = [
+        {
+            img: '/images/featured/placeholder-images-glasses.png',
+            lineOne: 'Glasses',
+            lineTwo: 'Custom shop',
+            linkTitle: 'Shop now',
+            linkTo: '/shop?categ=glasses',
+        },
+        {
+            img: '/images/featured/placeholder-images-watches.png',
+            lineOne: 'Watches',
+            lineTwo: 'Custom shop',
+            linkTitle: 'Shop now',
+            linkTo: '/shop?categ=watches',
+        },
+    ];
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+    };
+
+    const generateSlides = () => (
+        slides ?
+            slides.map((item, i)=>(
+                <div key={i}>
+                    <div className="featured_image"
+                    style={{
+                        background: `url(${item.img})`,
+                        height: `${window.innerHeight-85}px`
+                    }}>
+                        <div className="featured_action">
+                            <div className="tag title">{item.lineOne}</div>
+                            <div className="tag low_title">{item.lineTwo}</div>
+                            <div>
+                                <MyButton
+                                type="default"
+                                title={item.linkTitle}
+                                linkTo={item.linkTo}
+                                addStyles={{margin: '10px 0 0 0'}}/></div>
+                        </div>
+
+                    </div>
+                </div>
+            ))
+            : null
+    );
+
+    return (
+        <div className="featured_container">
+            <Slider {...settings}>
+                {generateSlides()}
+            </Slider>
+
+        </div>
+    );
+};
+
+export default HomeSlider;
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomeSlider from './HomeSlider'
 
 class Home extends React.Component{
     render(){
       return(
           <div>
-                home
+                <HomeSlider/>
           </div>
       );
     }
